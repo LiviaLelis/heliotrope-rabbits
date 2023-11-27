c29c7455fb8995762cbccbc4a49d4dd0f9bf73e2
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2023-11-27 09:40:19 -0300

    slight improvements

--- sequential.c
+++ sequential.c
@@ -1,40 +1,63 @@
-// Versão Sequencial do cálculo das Distâncias Euclidianas e de Manhathan.
-// Encontra as menores e maiores distâncias de cada medida.
-// Encontra os somatórios das menores e maiores distâncias de cada medida e de cada ponto.
-//
-// O padrão de entrada dos dados deste algoritmo sequencial difere do padrão solicitado no trabalho.
-// Siga a especificação dada para o trabalho.
-//
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <limits.h>
 #include <float.h>
 
-// Ordem das matrizes quadradas. 
-// O valor de N está fixo nesta versão sequencial. 
-// Para a versão paralela, neste e nos demais casos, siga todas as especificações passadas para o trabalho.
-#define N 4 
+// Debug Control //
 
-// Calcula a distância de Manhattan entre dois pontos
-int manhattan_distance(int x1, int y1, int z1, int x2, int y2, int z2) 
-{
-    return abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2);
-}
+#define DEBUG 0
 
-// Calcular a distância Euclidiana entre dois pontos
-double euclidean_distance(int x1, int y1, int z1, int x2, int y2, int z2) 
-{
-    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2) + pow(z1 - z2, 2));
-}
+#ifdef DEBUG
+#define dbg_print(fmt, ...) do { \
+if (DEBUG) { \
+fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt, \
+__FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+} \
+} while (0)
+#else
+#define dbg_print(fmt, ...) do {} while (0)
+#endif
 
-int main() 
+#ifdef DEBUG
+#define dbg_print_clean(fmt, ...) do { \
+if (DEBUG) { \
+fprintf(stderr, fmt, ##__VA_ARGS__); \
+} \
+} while (0)
+#else
+#define dbg_print_clean(fmt, ...) do {} while (0)
+#endif
+
+#define errprintf(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
+
+// Declarations //
+
+int manhattan_distance(int x1, int y1, int z1, int x2, int y2, int z2);
+double euclidean_distance(int x1, int y1, int z1, int x2, int y2, int z2);
+void parse_args(int argc, char* argv[], unsigned* n, unsigned* seed);
+
+int main(int argc, char* argv[])
 {
-    // Criação das matrizes para as coordenadas X, Y e Z
-	// A maneira de criar as matrizes e inserir os dados nelas foi simplificada nesta versão sequencial. 
-	// Para a versão paralela, neste e nos demais casos, siga todas as especificações passadas para o trabalho.
-	int x[] = {16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
-	int y[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-	int z[] = {1,5,9,13,2,6,10,14,3,7,11,15,4,8,12,16};
+	unsigned n, seed;
+	parse_args(argc, argv, &n, &seed);
+
+	int *x = malloc(n * n * sizeof(int));
+	int *y = malloc(n * n * sizeof(int));
+	int *z = malloc(n * n * sizeof(int));
+
+	int* targets[3] = {
+		x,
+		y,
+		z
+	};
+
+	srand(seed);
+	for(int o = 0; o < 3; o++) {
+		for(int i = 0; i < n * n; i++) {
+			targets[o][i] = rand() % 100;
+		}
+	}
 
 	int ij, k;
 	int manhattan_dist;
@@ -45,15 +68,16 @@ int main()
     double min_euclidean_per_point, min_euclidean = DBL_MAX, sum_min_euclidean = 0.0;
     double max_euclidean_per_point, max_euclidean = 0.0, sum_max_euclidean = 0.0;
 
-    // Calcule as distâncias de Manhattan e Euclidiana e atualize os valores mínimo e máximo
-    for (ij = 0; ij < N*N; ij++) 
+	// Compute the solution for each point, then reduce
+    for (ij = 0; ij < n*n; ij++)
 	{
 		min_manhattan_per_point = INT_MAX;
 		max_manhattan_per_point = 0;
 		min_euclidean_per_point = DBL_MAX;
 		max_euclidean_per_point = 0;
-				
-		for (k = ij+1; k < N*N; k++) 
+
+		// Compute sub-solutions to each point
+		for (k = ij+1; k < n*n; k++)
 		{
             manhattan_dist = manhattan_distance(x[ij], y[ij], z[ij], x[k], y[k], z[k]);
             euclidean_dist = euclidean_distance(x[ij], y[ij], z[ij], x[k], y[k], z[k]);
@@ -79,9 +103,9 @@ int main()
                 max_euclidean_per_point = euclidean_dist;
 			}
             
-        } // fim for k
+        }
 
-		// acerta os mínimos e os máximos globais
+		// Reduce
 		if (min_manhattan_per_point < min_manhattan)
             min_manhattan = min_manhattan_per_point;
 
@@ -96,20 +120,47 @@ int main()
 			
 		if (min_manhattan_per_point != INT_MAX && min_euclidean_per_point != DBL_MAX)
 		{
-			// terminou um ponto de origem (um i,j). Agora soma as distancias min e max de cada métrica, deste ponto de origem
 			sum_min_manhattan += min_manhattan_per_point;
 			sum_max_manhattan += max_manhattan_per_point;
 			sum_min_euclidean += min_euclidean_per_point;
 			sum_max_euclidean += max_euclidean_per_point;
 		}
-	
-		//printf("Dist Manhattan ponto {%d,%d,%d}: min: %d e máx: %d.\n", x[ij], y[ij], z[ij], min_manhattan_per_point, max_manhattan_per_point);
-		//printf("Dist Euclidiean ponto {%d,%d,%d}: min: %.2lf e máx: %.2lf.\n", x[ij], y[ij], z[ij], min_euclidean_per_point, max_euclidean_per_point);  
-        
-    } // fim for ij
+    }
 
     printf("Distância de Manhattan mínima: %d (soma min: %d) e máxima: %d (soma max: %d).\n", min_manhattan, sum_min_manhattan, max_manhattan, sum_max_manhattan);
     printf("Distância Euclidiana mínima: %.2lf (soma min: %.2lf) e máxima: %.2lf (soma max: %.2lf).\n", min_euclidean, sum_min_euclidean, max_euclidean, sum_max_euclidean);
   
     return 0;
-} // fim da main() 
+}
+
+int manhattan_distance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2);
+}
+
+double euclidean_distance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2) + pow(z1 - z2, 2));
+}
+
+void parse_args(int argc, char* argv[], unsigned* n, unsigned* seed) {
+    // Load matrix rank
+    if (argc >= 2) {
+        *n = strtoul(argv[1], NULL, 10);
+        dbg_print("Received CLI arg n=%u\n", *n);
+    }
+    else {
+        *n = 10;
+        dbg_print("Using fallback CLI arg n=%u\n", *n);
+    }
+
+    // Load RNG seed
+    if (argc >= 3) {
+        *seed = strtoul(argv[2], NULL, 10);
+        dbg_print("Received CLI arg seed=%u\n", *seed);
+    }
+    else {
+        *seed = 1;
+        dbg_print("Using fallback CLI arg seed=%u\n", *seed);
+    }
+}
